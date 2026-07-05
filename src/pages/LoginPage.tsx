1f647bc7588803a4f6688a911f2e3be95d83c672
vipin760 <vipinm500@gmail.com>
2026-07-06 01:28:38 +0530

    order section completed

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import { 
-  Zap, 
   Mail, 
   Lock, 
   Eye, 
